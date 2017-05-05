2d8d8644773125a29d0578f42eedd7339de40b98
jyacellyetr <jose.yacelly@endetransmision.bo>
2017-05-05 00:07:26 -0400

    Agregando los catalogos

--- base/data000001.sql
+++ base/data000001.sql
@@ -14,3 +14,17 @@ select pxp.f_insert_tgui ('Planificación', 'Planificación', 'SIGEFOP', 'si', 1
 select pxp.f_insert_tgui ('Competencias', 'Competencias', 'SIGEFOCO', 'si', 2, 'sis_formacion/vista/competencia/Competencia.php', 2, '', 'Competencia', 'SIGEFO');
 
 /********************************************F-DAT-YAC-SP-0-05/05/2017********************************************/
+
+/********************************************I-DAT-YAC-SP-1-05/05/2017********************************************/
+
+
+select param.f_import_tcatalogo_tipo ('insert','tplanificacion_critico','SIGEFO','tplanificacion_critico');
+select param.f_import_tcatalogo ('insert','SIGEFO','criterio 01','cri01','tplanificacion_critico');
+select param.f_import_tcatalogo ('insert','SIGEFO','criterio 02','cri02','tplanificacion_critico');
+
+select param.f_import_tcatalogo_tipo ('insert','tipocompetencia','SIGEFO','tcompetencia');
+select param.f_import_tcatalogo ('insert','SIGEFO','Conocimiento','conocimiento','tipocompetencia');
+select param.f_import_tcatalogo ('insert','SIGEFO','Cualidad','cualidad','tipocompetencia');
+select param.f_import_tcatalogo ('insert','SIGEFO','Actitud','actitud','tipocompetencia');
+
+/********************************************F-DAT-YAC-SP-1-05/05/2017********************************************/
